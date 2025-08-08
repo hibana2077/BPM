9416240904ee2d214b1cd923366bf1b1cd29c9e5
hibana2077 <hibana2077@gmail.com>
2025-08-08 22:22:36 +0800

    fix: Update PBS queue and resource settings in ct80_swin_base_384_ep50.sh

--- scripts/ct80_swin_base_384_ep50.sh
+++ scripts/ct80_swin_base_384_ep50.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #PBS -P rp06
-#PBS -q gpuvolta
-#PBS -l ngpus=1            
-#PBS -l ncpus=12            
-#PBS -l mem=20GB           
+#PBS -q dgxa100
+#PBS -l ngpus=1
+#PBS -l ncpus=16
+#PBS -l mem=32GB
 #PBS -l walltime=00:30:00
-#PBS -l wd                  
+#PBS -l wd
 #PBS -l storage=scratch/rp06
 
 module load cuda/12.6.2
 # module load python3/3.10.4
 
-nvidia-smi >> gpu-info-v100.txt
+nvidia-smi >> gpu-info-a100.txt
 source /scratch/rp06/sl5952/BPM/.venv/bin/activate
 
 cd ..
